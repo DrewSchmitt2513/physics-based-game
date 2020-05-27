597bd189a6326e07521d040987e12235bbce5d03
drew <ams2513@iastate.edu>
2020-05-27 00:00:49 -0500

    Beginning work on touch/gesture events that will act as the framework for manipulating the game.

--- App/app/src/main/java/com/example/physicsbasedgame/GameView.java
+++ App/app/src/main/java/com/example/physicsbasedgame/GameView.java
@@ -4,14 +4,23 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Toast;
 
-class GameView extends SurfaceView implements SurfaceHolder.Callback {
+import androidx.core.view.GestureDetectorCompat;
+
+class GameView extends SurfaceView implements SurfaceHolder.Callback, GestureDetector.OnGestureListener {
+
+    private static final String DEBUG_TAG = "Gestures";
 
     private GameThread thread;
     private Player player;
     private Paint paint;
+    private GestureDetectorCompat detectorCompat;
 
     private long left;
     private long right;
@@ -47,15 +56,11 @@ class GameView extends SurfaceView implements SurfaceHolder.Callback {
         player = new Player(left, top, right, bottom);
         paint = new Paint();
 
-
+        detectorCompat = new GestureDetectorCompat(getContext(), this);
     }
-
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        thread.setRunning(true);
-        thread.start();
-
         top = getBottom() - 100;
         bottom = getBottom() - 50;
         left = 500;
@@ -90,13 +95,17 @@ class GameView extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
 
+
+
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
         if (canvas != null) {
 
             canvas.drawColor(Color.BLACK);
-            paint.setColor(Color.RED);
+
+            if (hitBottom) paint.setColor(Color.BLUE);
+            else paint.setColor(Color.RED);
 
 
             canvas.drawRect(player, paint);
@@ -106,11 +115,12 @@ class GameView extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void onDraw(Canvas canvas) {
         if (!hitBottom) {
-//            System.out.println(leftRight + " " + hitTop);
+
             if (player.right >= getRight()) leftRight = false;
             else if (player.left <= getLeft()) leftRight = true;
             else if (player.bottom >= getBottom()) hitBottom = true;
             else if (player.top <= getTop()) hitTop = true;
+
             paint.setColor(Color.YELLOW);
             if (leftRight) {
                 if (!hitTop) goUpRight();
@@ -119,6 +129,7 @@ class GameView extends SurfaceView implements SurfaceHolder.Callback {
                 if (!hitTop) goUpLeft();
                 else goDownLeft();
             }
+
             canvas.drawRect(player, paint);
         } else {
             player.offsetTo(left, getBottom() - 50);
@@ -154,5 +165,56 @@ class GameView extends SurfaceView implements SurfaceHolder.Callback {
         top += deltaY + (deltaY * gravity);
 
         player.offsetTo(left, top);
+    }
+
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        if (this.detectorCompat.onTouchEvent(e)) {
+            return true;
+        }
+        return super.onTouchEvent(e);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        Log.d(DEBUG_TAG,"onDown: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+
+        if (player.contains(event1.getX(), event1.getY())) {
+            Toast.makeText(getContext(), "TOUCHED", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX,
+                            float distanceY) {
+        Log.d(DEBUG_TAG, "onScroll: " + event1.toString() + event2.toString());
+        return true;
     }
 }
